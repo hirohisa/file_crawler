739aa57175df728c2365f434d6ab03e3aae8a211
Hirohisa Kawasaki <hirohisa.kawasaki@gmail.com>
2016-05-05 18:29:34 +0900

    Refactor - Create directory `command`

--- lib/file_crawler/finder.rb
+++ lib/file_crawler/finder.rb
@@ -1,96 +1,23 @@
+require 'file_crawler/finder/command'
+
 module FileCrawler
 
   class Finder
     attr_accessor :path
+    attr_reader :rows
 
-    # directory [Boolean]
-    # exntesion [Array]
+    include Command::Search
+
     def initialize(path)
       @path = path
       select_in_path(path)
     end
-
-    def rows
-      @rows ||= []
-    end
-
-    def select_in_path(path)
-      tap {
-        @rows = find_rows_in_path(path)
-      }
-    end
-
-    def find_rows_in_path(path)
-      Dir.entries(path).select {|item|
-        !item.start_with?('.')
-      }.map {|item|
-        path + '/' + item
-      }
-    end
-
-    def select_with_extension(extension)
-      tap {
-        @rows = find_rows_with_extension(extension, rows)
-      }
-    end
-
-    def find_rows_with_extension(extension, resource)
-      find_rows_files(resource).select {|item|
-        extension.any? {|e|
-          extname = File.extname(item).downcase
-          extname == ".#{e.downcase}"
-        }
-      }
-    end
-
-    def select_files
-      tap {
-        @rows = find_rows_files(rows)
-      }
-    end
-
-    def find_rows_files(resource)
-      resource.select {|item|
-        !File.directory?(item)
-      }
-    end
-
-    def select_directories
-      tap {
-        @rows = find_rows_directories(rows)
-      }
-    end
-
-    def find_rows_directories(resource)
-      resource.select {|item|
-        File.directory?(item)
-      }
-    end
-
-    def select_with_extension_in_directory(extension)
-      tap {
-        @rows = find_rows_with_extension_in_directories(extension, find_rows_directories(rows))
-      }
-    end
-
-    def find_rows_with_extension_in_directories(extension, directories)
-      directories.select {|directory|
-        variable_to_exist_with_extension_in_directory(extension, directory)
-      }
-    end
-
-    def variable_to_exist_with_extension_in_directory(extension, directory)
-      subfiles = find_rows_in_path(directory)
-      result = find_rows_with_extension(extension, subfiles)
-      return true if result.size > 0
-
-      subdirectories = find_rows_directories(subfiles).select {|subdirectory|
-        variable_to_exist_with_extension_in_directory(extension, subdirectory)
-      }
-      subdirectories.size > 0
-    end
   end
 
+  # @param conditions
+  #   directory [Boolean]
+  #   exntesion [Array]
+  #   extension_in_directory [Array]
   def self.search(path, conditions = {})
     finder = FileCrawler::Finder.new(path)
 
@@ -107,5 +34,7 @@ module FileCrawler
 
     finder.rows
   end
+
+
 
 end
