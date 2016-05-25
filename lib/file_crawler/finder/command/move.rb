df510029efded15a38d020ff05c015252389e96c
Hirohisa Kawasaki <hirohisa.kawasaki@gmail.com>
2016-05-26 03:22:21 +0900

    Update finder#move with conditions `:numbering`

--- lib/file_crawler/finder/command/move.rb
+++ lib/file_crawler/finder/command/move.rb
@@ -5,7 +5,7 @@ module FileCrawler
     module Command
       module Move
 
-        def move(directories, destination)
+        def move_directories_not_exist_destination(directories, destination)
           move_targets = directories.select {|directory|
             valiable_to_move?(directory, destination)
           }
@@ -15,17 +15,74 @@ module FileCrawler
 
           FileUtils.mv(move_targets, destination)
 
-          move_targets.map {|path|
-            destination + '/' + File.basename(path)
+          @rows = move_targets.map {|directory|
+            destination + '/' + File.basename(directory)
           } + not_move_targets
         end
 
-        def valiable_to_move?(current_path, destination)
-          return false if current_path == destination
+        def move_directories_with_numbering(directories, destination)
+          move_targets = []
+          not_move_targets = []
+          rename_targets = []
 
-          next_path = destination + '/' + File.basename(current_path)
-          !File.exist?(next_path)
+          directories.each {|directory|
+            if is_same?(directory, destination)
+              not_move_targets << directory
+              next
+            end
+
+            if exist_file?(directory, destination)
+              rename_targets << directory
+              next
+            end
+
+            move_targets << directory
+          }
+
+          FileUtils.mv(move_targets, destination)
+
+          renamed_targets = []
+          rename_targets.each {|directory|
+            filename = find_free_filename(directory, destination)
+            to = destination + '/' + filename
+            FileUtils.mv(directory, to)
+
+            renamed_targets << to
+          }
+
+          @rows = move_targets.map {|directory|
+            destination + '/' + File.basename(directory)
+          } + not_move_targets + renamed_targets
         end
+
+        def find_free_filename(current, destination)
+          filename = File.basename(current)
+
+          index = 1
+          new_filename = "#{filename} (#{index})"
+          while File.exist?(new_filename)
+            i += 1
+            new_filename = "#{filename} (#{index})"
+          end
+
+          new_filename
+        end
+
+        def valiable_to_move?(current, destination)
+          return false if is_same?(current, destination)
+
+          !exist_file?(current, destination)
+        end
+
+        def is_same?(current, destination)
+          current == destination
+        end
+
+        def exist_file?(current, destination)
+          next_path = destination + '/' + File.basename(current)
+          File.exist?(next_path)
+        end
+
       end
     end
   end
