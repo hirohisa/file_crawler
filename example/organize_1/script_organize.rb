754f8936b63ac53fad37f1322829c2ea6682c9f3
Hirohisa Kawasaki <hirohisa.kawasaki@gmail.com>
2018-04-21 13:03:55 +0900

    Remove example data

--- example/organize_1/script_organize.rb
+++
@@ -1,7 +0,0 @@
-$LOAD_PATH.unshift File.expand_path('../../../lib', __FILE__)
-require 'file_crawler'
-
-root = Dir.getwd # repository's root directory
-directory = root + '/example/organize_1/data/source'
-destination = root + '/example/organize_1/data/destination'
-FileCrawler.organize([directory], destination)
--- example/search_1/script_search.rb
+++
@@ -1,8 +0,0 @@
-$LOAD_PATH.unshift File.expand_path('../../../lib', __FILE__)
-require 'file_crawler'
-
-root = Dir.getwd # repository's root directory
-directory = root + '/example/search_1'
-result = FileCrawler.search(directory)
-
-puts result
